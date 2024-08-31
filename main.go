1f06968b65bb0ae39b3b4d34a7bdda88388fa172
afrisanjaya <47937342+afrisanjaya@users.noreply.github.com>
2024-08-31 13:31:34 +0700

    multistage

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Add(a, b int) int {
 	return a + b
@@ -15,5 +20,28 @@ func Multiply(a, b int) int {
 }
 
 func main() {
-	fmt.Println("Hello, Go!")
+	r := gin.Default()
+
+	r.GET("/add/:a/:b", func(c *gin.Context) {
+		a, _ := strconv.Atoi(c.Param("a"))
+		b, _ := strconv.Atoi(c.Param("b"))
+		result := Add(a, b)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+
+	r.GET("/subtract/:a/:b", func(c *gin.Context) {
+		a, _ := strconv.Atoi(c.Param("a"))
+		b, _ := strconv.Atoi(c.Param("b"))
+		result := Subtract(a, b)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+
+	r.GET("/multiply/:a/:b", func(c *gin.Context) {
+		a, _ := strconv.Atoi(c.Param("a"))
+		b, _ := strconv.Atoi(c.Param("b"))
+		result := Multiply(a, b)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+
+	r.Run(":8181")
 }
--- main_test.go
+++ main_test.go
@@ -1,27 +1,67 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/add/:a/:b", func(c *gin.Context) {
+		a, _ := strconv.Atoi(c.Param("a"))
+		b, _ := strconv.Atoi(c.Param("b"))
+		result := Add(a, b)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+	r.GET("/subtract/:a/:b", func(c *gin.Context) {
+		a, _ := strconv.Atoi(c.Param("a"))
+		b, _ := strconv.Atoi(c.Param("b"))
+		result := Subtract(a, b)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+	r.GET("/multiply/:a/:b", func(c *gin.Context) {
+		a, _ := strconv.Atoi(c.Param("a"))
+		b, _ := strconv.Atoi(c.Param("b"))
+		result := Multiply(a, b)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+	return r
+}
 
 func TestAdd(t *testing.T) {
-	result := Add(1, 1)
-	expected := 2
-	if result != expected {
-		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
-	}
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/add/1/1", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, `{"result":2}`, w.Body.String())
 }
 
 func TestSubtract(t *testing.T) {
-	result := Subtract(2, 2)
-	expected := 0
-	if result != expected {
-		t.Errorf("Subtract(5, 3) = %d; want %d", result, expected)
-	}
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/subtract/2/2", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, `{"result":0}`, w.Body.String())
 }
 
 func TestMultiply(t *testing.T) {
-	result := Multiply(3, 3)
-	expected := 9
-	if result != expected {
-		t.Errorf("Multiply(4, 3) = %d; want %d", result, expected)
-	}
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/multiply/3/3", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, `{"result":9}`, w.Body.String())
 }
